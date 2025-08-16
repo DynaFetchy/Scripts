c28243374b5cf2974febe7bb411942cad0c266a7
DynaFetchy <147998370+DynaFetchy@users.noreply.github.com>
2025-08-16 17:10:05 +0200

    Create PetFinder.lua

---
+++ PetFinder.lua
@@ -0,0 +1,17 @@
+--// Variables
+local scriptUrls = {
+    [96342491571673] = "PetFinder.lua",
+    [109983668079237] = "PetFinder.lua"
+}
+
+local scriptUrl = scriptUrls[game.PlaceId]
+if scriptUrl then
+    local success, errorMsg = pcall(function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/iubauhqhuiauhiszivjou1i89781iuwnuk8p/-/refs/heads/main/" .. scriptUrl))()
+    end)
+    if not success then
+        warn("[Devil Ugly] Failed to load script:", errorMsg)
+    end
+else
+    print("[Devil Ugly] Game ID not found in script URLs table.")
+end
